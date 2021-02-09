1404dfcb45a29aff5b1df43a42391e726afede8d
matsu <info@matsuwork.shop>
2021-02-09 12:35:50 +0900

    arrange css

--- work17/js/script.js
+++ work17/js/script.js
@@ -3,7 +3,7 @@ const object = {
         jp: "ニュース",
         data: [
             {title: "ニュースの記事タイトル１", new: true, comment:0, url:"news1.html"},
-            {title: "ニュースの記事タイトル２", new: false, comment:1, url:"news2.html"},
+            {title: "ニュースの記事タイトル２", new: true, comment:1, url:"news2.html"},
             {title: "ニュースの記事タイトル３", new: false, comment:3, url:"news3.html"},
             {title: "ニュースの記事タイトル４", new: false, comment:10, url:"news4.html"},
         ],
@@ -13,9 +13,9 @@ const object = {
         jp: "経済",
         data: [
             {title: "経済の記事タイトル１", new: true, comment:0, url:"economy1.html"},
-            {title: "経済の記事タイトル２", new: false, comment:3, url:"economy1.html"},
-            {title: "経済の記事タイトル３", new: false, comment:7, url:"economy1.html"},
-            {title: "経済の記事タイトル４", new: true, comment:0, url:"economy1.html"},
+            {title: "経済の記事タイトル２", new: false, comment:9, url:"economy1.html"},
+            {title: "経済の記事タイトル３", new: false, comment:33, url:"economy1.html"},
+            {title: "経済の記事タイトル４", new: true, comment:4, url:"economy1.html"},
         ],
         img: "img/economy.png"
     }
@@ -35,23 +35,31 @@ if(sessionStorage.getItem('selectedCategory')){
 }
 
 const maxNum = 4;
+const container = document.querySelector('.container');
+
 const ul = document.querySelector('ul');
+ul.classList.add('contents');
 const img = document.createElement('img');
+const menuUl = document.createElement('ul');
+menuUl.classList.add('menu');
 
 function writeMenu(obj) {
-    let menuUl = document.createElement('ul');
-
     Object.keys(obj).forEach(function (key) {
         let menuLi = document.createElement('li');
+        menuLi.classList.add('menu__list');
         menuLi.textContent = obj[key].jp;
         menuLi.addEventListener('click', function(){
             sessionStorage.selectedCategory = key
+            for (let i = 0 ; i < menuUl.children.length ; i++){
+                menuUl.children.item(i).classList.remove('is-active');
+            }
+            menuLi.classList.add('is-active');
             writeLists(obj[key])
         }, false);
         menuUl.appendChild(menuLi);
     });
 
-    ul.before(menuUl)
+    container.before(menuUl)
 }
 
 function writeLists(obj) {
@@ -62,11 +70,13 @@ function writeLists(obj) {
     for (var i = 0 ; i < array.length ; i++) {
 
         let li = document.createElement('li');
+        li.classList.add('contents__list');
         let a = document.createElement('a');
         a.innerHTML = array[i].title;
         a.href = array[i].url;
         li.appendChild(a);
         let newSpan = document.createElement('span');
+        newSpan.classList.add('new');
         newSpan.textContent = 'NEW'
 
         if(array[i].new){
@@ -75,16 +85,18 @@ function writeLists(obj) {
 
         if(array[i].comment > 0){
             let comSpan = document.createElement('span');
-            comSpan.textContent = array[i].comment
+            comSpan.textContent = array[i].comment;
+            comSpan.classList.add('comment');
             li.appendChild(comSpan);
         }
 
-        fragment.appendChild(li)
+        fragment.appendChild(li);
     }
 
     ul.appendChild(fragment)
 
-    img.src= obj.img;
+    img.src = obj.img;
+    img.alt = `${obj.jp}の画像`
     ul.after(img)
 };
 
@@ -92,6 +104,8 @@ async function tryOnLoad() {
     try {
         let resObject = await getObject();
         writeMenu(resObject);
+        let i = Object.keys(resObject).findIndex( value => value == cat );
+        menuUl.children.item(i).classList.add('is-active');
         writeLists(resObject[cat])
     } catch (err) {
         console.error(err);
