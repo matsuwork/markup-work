d553366cfcc0ad805eb5ba063072f966a5ae856e
matsu <info@matsuwork.shop>
2021-04-10 22:04:14 +0900

    Don't create button

--- new_work26/js/logout.js
+++ new_work26/js/logout.js
@@ -1,16 +1,5 @@
-function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-window.addEventListener('load', async function(){
-    await timeout(3000);
-    const body = document.querySelector('body')
-    const button = document.createElement('button');
-    button.classList.add('logout');
-    button.textContent = 'Log out';
-    button.addEventListener('click',function(){
-        localStorage.clear();
-        location.href="login.html";
-    },false);
-    body.insertBefore(button, body.firstChild)
+logoutButton = document.getElementById('js-logout');
+logoutButton.addEventListener('click',function(){
+    localStorage.clear();
+    location.href="login.html";
 },false);
